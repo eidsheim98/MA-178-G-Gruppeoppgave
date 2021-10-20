85d3323b926f60879f04349649bfa952e9656b03
Nikolai Bunch Eidsheim <nikolaie@uia.no>
2021-10-20 11:13:15 +0200

    Lagt til readme.md og requirements.txt

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import os
 import subprocess
-import threading
-
+import platform
 
 def run_file(filename):
+    """
+    Kjører spesifisert fil hvis funnet i hierarkiet
+    :param filename: Filnavnet uten .py
+    :return: None
+    """
     found = False
+    # Se etter fila
     for path, subdirs, files in os.walk(os.getcwd() + '/matte'):
+        # For hvert filnavn funnet
         for name in files:
+            # Hvis filnavnet stemmer med filnavet som ble skrevet inn
             if name == filename:
+                # Sett fila som funnet
                 fullpath = path + '/' + name
                 found = True
                 break
         if found:
             break
 
+    # Hvis fila ble funnet, kjør fila
     if found:
         print("Running " + filename + '\n')
-        #os.system('start /wait cmd /k python ' + fullpath)
-        subprocess.Popen(['python', fullpath])
+
+        # Hvis man er på windows, kjør python kommando, hvis ikke kjør python3 kommando
+        # Usikker på om dette fungerer i MacOS, men kjører samme kommando som Linux fordi begge er UNIX
+        if 'windows' in platform.system().lower():
+            subprocess.Popen(['python', fullpath])
+        else:
+            subprocess.Popen(['python3', fullpath])
+
+    # Hvis fila ikke ble funnet, gi beskjed om dette
     else:
         print("Could not find " + filename + '\n')
 
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Matteoppgave MA-178
+
+## Installasjon
+
+Klon prosjektet ved å skrive 
+
+```bash
+git clone https://github.com/eidsheim98/MA-178-G-Gruppeoppgave.git
+```
+
+cd inn i mappa
+
+```bash
+cd MA-178-G-Gruppeoppgave.git
+```
+
+Kjør installasjon av python pakker
+
+```bash
+pip install -r requirements.txt
+```
+
+##Bruk
+
+Du kan velge å kjøre hver fil for seg, eller bruke menyen ved å kjøre fila 
+```bash
+main.py
+```
+
+Startes enten i pycharm eller med denne kommandoen i cmd
+
+```bash
+python main.py
+```
+
+Så skriver du inn filnavnet på den fila du vil kjøre, uten ".py".
+
+###Eksempel
+For å kjøre fil 2a1.py (Oppgave 2a, første plot), skriv
+```bash
+2a1
+```
+
